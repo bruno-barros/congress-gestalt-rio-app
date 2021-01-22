6659c2ca5347b7affa060c0ab3aa0f0945c2d970
Bruno <dev@conceito-online.com.br>
2021-01-22 20:53:06 -0300

    fix redirect

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -32,6 +32,7 @@ const Dashboard = (props: DashboardProps) => {
    * -----------------------------
    */
   useEffect(()=>{
+    if(!user) return
     if(user.canManageAbstracts()) router.push(`/adm/abstracts`)
     else router.push(`/abstracts`)
   }, [user])
