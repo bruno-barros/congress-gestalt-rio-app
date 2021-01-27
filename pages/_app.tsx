78390f00afea25657ccc4543b93631166347f61e
Bruno <dev@conceito-online.com.br>
2021-01-27 12:03:16 -0300

    google analytics 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import {ReactQueryDevtools} from "react-query/devtools";
 import {QueryClient, QueryClientProvider} from "react-query";
 import '../src/i18n'
 import {ToastContainer} from "react-toastify";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
+import * as gtag from '../src/gtag'
 
 const stored = store();
 
@@ -18,6 +21,18 @@ const queryClient = new QueryClient({
 });
 
 export default function App({Component, pageProps}) {
+
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Provider store={stored}>
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,9 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.GA_ID}');
+            gtag('config', '${process.env.GA_ID}',{
+              page_path: window.location.pathname,
+            });
         `,
             }}
           />
