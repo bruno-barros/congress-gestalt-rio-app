7649fcafa3c0e9b514f37dbd6a0b527b69f7cf70
Bruno Barros <web@conceito-online.com.br>
2021-07-30 10:26:02 -0300

    fix product null

--- components/order/order-line.tsx
+++ components/order/order-line.tsx
@@ -17,11 +17,11 @@ export default function OrderLine(props: OrderLineProps) {
       <div className="">{moment(order.date).format('DD/MM/YYYY H:mm')}</div>
     </div>
     <div className="pt-3 pb-1" style={{fontSize: '1.4em'}}>
-      {order.getItems().map(line => (<div key={line.product.databaseId} className="d-flex align-items-center">
+      {order.getItems().map(line => (<div key={line.product?.databaseId} className="d-flex align-items-center">
         <ToolTip text={order.status}>
           <div className={`status-ball ${order.status.toLowerCase()}`}></div>
         </ToolTip>
-        <strong className="mx-4">{line.product.name}</strong>
+        <strong className="mx-4">{line.product?.name}</strong>
         <div className="">{line.product?.productCategories?.nodes[0]?.name}</div>
       </div>))}
     </div>
