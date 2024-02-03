18d2b28d3429f8a9cd2ca23eb36a91b62f03f627
Bruno Barros <web@conceito-online.com.br>
2024-02-03 13:57:00 -0300

    Add metadata fields to query in useSubscriptions hook

--- components/hooks/useSubscriptions.tsx
+++ components/hooks/useSubscriptions.tsx
@@ -18,6 +18,7 @@ export default function useSubscriptions(edition?: Edition) {
   function queryData(): Promise<OrderCollection|null> {
     return new Promise((resolve, reject) => {
       WpOrder.subscriptions({
+        metadata: ["is_pdc","pdc_needs","is_child_care", "allow_newsletter", "badge_name"],
         dateStart: {
           day: Number(s.format('DD')),
           month: Number(s.format('MM')),
