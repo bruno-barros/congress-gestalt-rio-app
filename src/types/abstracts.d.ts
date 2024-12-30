74d5edd12c3d79c40b0fcb11e4abbcb9632c0a9e
Bruno Barros <web@conceito-online.com.br>
2024-12-30 09:32:08 -0300

    adiciona enumeração de status abstrato e define interface para autores

--- src/types/abstracts.d.ts
+++ src/types/abstracts.d.ts
@@ -2,8 +2,18 @@
  * Usado para listar categorias e itens como opções de seleção
  */
 export interface KeyValueItem {
-    id: string;
-    pt: string;
-    en: string;
-    es: string;
+  id: string;
+  pt: string;
+  en: string;
+  es: string;
 }
+/**
+ * Define se o fluxo será completo: resumo + trabalho completo, ou apenas trabalho
+ */
+export enum AbstractStatusModelEnum {
+  SINOPSE_ABSTRACT = "sinopse_abstract",
+  ABSTRACT = "abstract",
+}
+
+export type StatusType = 'pending' | 'synopsis_revision' | 'synopsis_evaluating' | 'synopsis_rejected' | 'synopsis_waiting_upd' |
+  'synopsis_approved' | 'final_revision' | 'evaluating' | 'rejected' | 'waiting_update' | 'pre_approved' | 'approved';
