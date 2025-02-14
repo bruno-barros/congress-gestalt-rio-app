6c558197f84ba597c5668bdc1a73cc139d457aa4
Bruno Barros <web@conceito-online.com.br>
2025-02-14 16:45:16 -0300

    adiciona suporte para IDs de superusuários a partir de variáveis de ambiente

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ module.exports = {
     ONESIGNAL_ID: process.env.ONESIGNAL_ID,
     ONESIGNAL_SUBDOMAINNAME: process.env.ONESIGNAL_SUBDOMAINNAME,
     GA_ID: process.env.GA_ID,
-    production: isProd
+    production: isProd,
+    super_users_ids: process.env.SUPER_USERS_IDS,
   },
   // configuração de integração do i18n com Nextjs: router.locales
   // https://nextjs.org/docs/pages/building-your-application/routing/internationalization
