6d8be6bc50d04afe79e10088d03ff5bd994d989d
Bruno Barros <web@conceito-online.com.br>
2025-03-12 11:21:21 -0300

    adiciona validação de perfil completo para autores no painel de autores

--- components/abstract/authors/authors-panel.tsx
+++ components/abstract/authors/authors-panel.tsx
@@ -7,6 +7,7 @@ import s from "./authors-panel.module.scss"
 import SearchAuthor from "./search-author"
 import { dump } from "../../../src/helpers"
 import { useRouter } from "next/router"
+import useSettings from "../../hooks/useSettings"
 
 interface AuthorsPanelProps {
     abstractId: number|null|undefined
@@ -36,8 +37,19 @@ export default function AuthorsPanel(props: AuthorsPanelProps){
     const {abstractId, tempId, data: originalData, disabled, maxAuthors, mainAuthorId} = props
     const {data, setData, setAbstractId} = useAuthorsContext()
     const count = data.filter(a => a._active !== false).length
+    const { currentEdition: edition } = useSettings()
+  const AbstractCnf = edition?.Abstract()
+  const FieldAuthors2 = AbstractCnf?.getField('authors2')
+    const existsInvalid = data.map(a => isProfileComplete(a)).filter(a => !a).length > 0
     const isOnLimit = maxAuthors ? count >= maxAuthors : false
-// console.log({data, originalData})
+
+function isProfileComplete(author: AuthorSchema){
+    if(!author.name || !author.email) return false
+    if(FieldAuthors2.bio_max > 0 && !author.bio) return false
+    if(FieldAuthors2.bio2_max > 0 && !author.bio2) return false
+    if(FieldAuthors2.bio3_max > 0 && !author.bio3) return false
+    return true
+  }
     useEffect(()=>{
         const data = originalData.map((author) => {
             return {...author, _main: author.wp_user_id == mainAuthorId}
@@ -51,6 +63,11 @@ export default function AuthorsPanel(props: AuthorsPanelProps){
             <div className="label">{t('autores')}</div>
             {maxAuthors && <small>({t('trabalho.maximo-de')} {maxAuthors})</small>}
         </div>
+        {existsInvalid && 
+        <div className="alert alert-warning text-sm">
+            {t('autor.complete-informacoes')}
+        </div>}
+        
         <div className={s.list}>
             {/* {dump(data)} */}
             {(data.length > 0 && data.map((author) => {
