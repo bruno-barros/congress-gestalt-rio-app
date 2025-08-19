e103fa7f9caffb6a9f8f81b90fe86935a244d666
Bruno Barros <web@conceito-online.com.br>
2025-08-19 12:41:40 -0300

    feat: adicionar suporte a parâmetros opcionais para filtragem de atividades

--- components/hooks/activities/useActivities.tsx
+++ components/hooks/activities/useActivities.tsx
@@ -2,14 +2,18 @@ import { useQuery } from "react-query";
 import { ActivitySchema } from "../../../src/types/activity.type";
 import WpActivity from "../../../src/http/wp-activity";
 
-export default function useActivities(edition: string) {
+export default function useActivities(edition: string, args?: { active?: 0 | 1; user_id?: number | string }) {
   async function fetch(): Promise<ActivitySchema[]> {
-    const axios = await WpActivity.list({ edition });
+    const axios = await WpActivity.list({ 
+      edition: edition,
+      user_id: args?.user_id,
+      active: args?.active,
+    });
     const response = axios.data;
     return response.data;
   }
 
-  return useQuery(["activities", edition], fetch, {
+  return useQuery(["activities", edition, args], fetch, {
     enabled: !!edition,
   });
 }
