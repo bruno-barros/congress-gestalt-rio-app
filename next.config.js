358457c5f019918c68f3a78503148a0b20b4ee0c
Bruno Barros <web@conceito-online.com.br>
2026-01-12 09:28:07 -0300

    1.7.12

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   env: {
-    version: '1.7.11',
+    version: '1.7.12',
     apiUrl: process.env.API_BASEURL,
     GOOGLE_OAUTH_ID: process.env.GOOGLE_OAUTH_ID,
     FACEBOOK_OAUTH_ID: process.env.FACEBOOK_OAUTH_ID,
